e3026656bb85373714be7f90d538ff9ccc8e979d
Riyan315 <riyangaxys03@gmail.com>
2026-02-28 01:02:47 +0700

    feat: add JavaScript setup and mulOfArray function

---
+++ mulOfArray.js
@@ -0,0 +1,12 @@
+const arr1 = [2, 0, 26, 28, -2]
+function mulOfArray(arr) {
+    let result = 1
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+        result *= arr[i]
+        }
+    }
+    return result
+}
+const arr1Result = mulOfArray(arr1);
+console.log(mulOfArray(arr1))   
